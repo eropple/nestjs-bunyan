2aa29752d73cb81a053affe06ca8f62c09d42550
Ed Ropple <ed@edropple.com>
2019-10-20 15:51:18 -0400

    made imports of injector keys easier

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import * as _InjectorKeys from "./injector-keys";
 export const InjectorKeys = _InjectorKeys;
 
+export * from './injector-keys';
+
 export * from "./logging.module";
 export * from "./request-tracker.interceptor";
 export * from "./injector-decorations";
